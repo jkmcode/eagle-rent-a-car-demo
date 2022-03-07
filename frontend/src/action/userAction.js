e708375180c0d079608cd0db230f528481ce1644
jkmcode <jakub.kumala@jkmcode.eu>
2022-03-07 09:46:11 +0100

    Updated AWS credencials

--- frontend/src/action/userAction.js
+++ frontend/src/action/userAction.js
@@ -125,15 +125,26 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     }
 }
 
-export const createUser = (name ,email, password) => async (dispatch) => {
+export const createUser = (name ,email, password) => async (dispatch, getState) => {
     try{
         dispatch({
             type: USER_CREATE_REQUEST
         })
 
+        // const config = {
+        //     headers:{
+        //         'Content-type': 'application/json'
+        //     }
+        // }
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
         const config = {
             headers:{
-                'Content-type': 'application/json'
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
             }
         }
 
