93e08763c7548e6fb3d13278e81ac84ac1cbe0bc
jkmcode <jakub.kumala@jkmcode.eu>
2022-03-10 14:41:12 +0100

    'ALLOWED_HOSTS-added'

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'eagle-rent-car.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'eagle-rent-car.herokuapp.com', 'rent-car-demo.herokuapp.com']
 
 # Application definition
 
